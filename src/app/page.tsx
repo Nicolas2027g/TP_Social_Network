587712520989830f55065e597c7fd433ce8bb102
Nicolas Guillard <nguillard@et.esiea.fr>
2025-03-12 11:38:30 +0100

    DarkMode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ModeToggle from "@/components/ModdleToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
@@ -5,15 +7,17 @@ export default function Home() {
     <div>
       <SignedOut>
         <SignInButton mode="modal" >
-          <button className="bg-red-500">
+          <Button>
             Sign In
-          </button>
+          </Button>
         </SignInButton>
       </SignedOut>
 
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <ModeToggle/>
     </div>
   );
 }
