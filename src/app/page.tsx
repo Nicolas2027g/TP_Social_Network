17d8bd939b033b2437abd257e1aee3bdfeb1f543
Nicolas Guillard <nguillard@et.esiea.fr>
2025-03-12 11:22:06 +0100

    Shadcn start

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal" >
+          <button className="bg-red-500">
+            Sign In
+          </button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+}
