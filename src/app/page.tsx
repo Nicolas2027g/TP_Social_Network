e8fa6fce3b744fd05024454140a3c089511bb1c7
Nicolas Guillard <nguillard@et.esiea.fr>
2025-04-17 23:07:34 +0200

    database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+export default async function Home() {
 
-export default function Home() {
   return (
     <div>
 
